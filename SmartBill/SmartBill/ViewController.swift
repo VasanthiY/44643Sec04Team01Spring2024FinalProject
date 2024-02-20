71e51d621bd723dbfa76b299bf1d81ef88cbca9c
Krishna Vasanthi Yakkala <krishnavasanthiyakkala@gmail.com>
2024-02-19 21:17:53 -0600

    Created application

---
+++ SmartBill/SmartBill/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartBill
+//
+//  Created by Krishna Vasanthi on 2/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
