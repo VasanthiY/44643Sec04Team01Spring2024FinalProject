14009545be746800017d2ecc0afe06e46d9d6145
kalpana762 <kalpanakanakamalla9@gmail.com>
2024-02-23 09:49:29 -0800

    kalpana added previous bills page

---
+++ SmartBill/project Ui's/project Ui's/loginVCViewController.swift
@@ -0,0 +1,29 @@
+//
+//  loginVCViewController.swift
+//  project Ui's
+//
+//  Created by kalpana on 2/23/24.
+//
+
+import UIKit
+
+class loginVCViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
