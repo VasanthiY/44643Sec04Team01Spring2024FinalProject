5b3e0740395397781f8461e0dbc0c8f8eaab9feb
Krishna Vasanthi Yakkala <krishnavasanthiyakkala@gmail.com>
2024-03-05 21:17:41 -0600

    Updated UI of Login page

---
+++ SmartBill/SmartBill/DesignableView.swift
@@ -0,0 +1,37 @@
+//
+//  DesignableView.swift
+//  SmartBill
+//
+//  Created by Krishna Vasanthi on 3/5/24.
+//
+
+import UIKit
+
+@IBDesignable
+class DesignableView: UIView {
+
+    @IBInspectable var shadowColor: UIColor = UIColor.clear {
+        didSet {
+            layer.shadowColor = shadowColor.cgColor
+        }
+    }
+
+    @IBInspectable var shadowRadius: CGFloat = 0 {
+        didSet {
+            layer.shadowRadius = shadowRadius
+        }
+    }
+
+    @IBInspectable var shadowOpacity: CGFloat = 0 {
+        didSet {
+            layer.shadowOpacity = Float(shadowOpacity)
+        }
+    }
+
+    @IBInspectable var shadowOffsetY: CGFloat = 0 {
+        didSet {
+            layer.shadowOffset.height = shadowOffsetY
+        }
+    }
+
+}
