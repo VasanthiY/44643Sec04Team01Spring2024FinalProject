a42a02872ebe455be4a9c822637da32e2dfb8024
Sai Ramya Gajula <ramyaroyal1639@gmail.com>
2024-04-10 12:19:25 -0500

    working on code functionality of Bill Gen Screen

--- SmartBill/SmartBill/ViewControllers/BillGen.swift
+++ SmartBill/SmartBill/ViewControllers/BillGen.swift
@@ -13,17 +13,13 @@ class BillGen: UIViewController {
     var predictionText = ""
     let predictionsToShow = 1
     var selectedImage = UIImage()
-
+    
     @IBOutlet weak var BillBTN: UIButton!
     
     @IBOutlet weak var titleLBL: UILabel!
     
     
     @IBOutlet weak var prevBillBTN: UIButton!
-    
-    
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,7 +30,7 @@ class BillGen: UIViewController {
         
     }
     
-   
+    
     @IBAction func prevBillBTN(_ sender: UIButton) {
         
         present(photoPicker, animated: false)
@@ -43,10 +39,12 @@ class BillGen: UIViewController {
     @IBAction func BillGenBTN(_ sender: UIButton) {
     }
     
+}
     
     
     extension BillGen {
       
+
        func userSelectedPhoto(_ photo: UIImage) {
           
            self.selectedImage = photo
@@ -56,6 +54,64 @@ class BillGen: UIViewController {
            }
        }
 
+    }
+
+    extension BillGen {
+        
+       private func classifyImage(_ image: UIImage) {
+           do {
+               try self.imagePredictor.makePredictions(for: image,
+                                                       completionHandler: imagePredictionHandler)
+           } catch {
+               print("Vision was unable to make a prediction...\n\n\(error.localizedDescription)")
+           }
+       }
+
+      
+       private func imagePredictionHandler(_ predictions: [ImagePredictor.Prediction]?) {
+           guard let predictions = predictions else {
+               
+               DispatchQueue.main.async { [self] in
+                   let vc = self.storyboard?.instantiateViewController(withIdentifier: "ItemDetailsVC") as! ItemDetailsVC
+                   vc.image = selectedImage
+                   vc.predictionText = "No Details"
+                   self.navigationController?.pushViewController(vc, animated: true)
+                  
+               }
+               
+               return
+           }
+
+           let formattedPredictions = formatPredictions(predictions)
+
+           let predictionString = formattedPredictions.joined(separator: "\n")
+           
+           DispatchQueue.main.async {
+               let vc = self.storyboard?.instantiateViewController(withIdentifier: "ItemDetailsVC") as! ItemDetailsVC
+               vc.image = self.selectedImage
+               vc.predictionText = predictionString
+               self.navigationController?.pushViewController(vc, animated: true)
+              
+           }
+           
+       }
+
+     
+       private func formatPredictions(_ predictions: [ImagePredictor.Prediction]) -> [String] {
+           // Vision sorts the classifications in descending confidence order.
+           let topPredictions: [String] = predictions.prefix(predictionsToShow).map { prediction in
+               var name = prediction.classification
+
+               
+               if let firstComma = name.firstIndex(of: ",") {
+                   name = String(name.prefix(upTo: firstComma))
+               }
+
+               return "\(name)"
+           }
+
+           return topPredictions
+       }
     }
 
     /*
@@ -68,8 +124,97 @@ class BillGen: UIViewController {
     }
     */
 
+
+
+import UIKit
+
+class HomeVC: UIViewController {
+  
+   let imagePredictor = ImagePredictor()
+   var predictionText = ""
+   let predictionsToShow = 1
+   var selectedImage = UIImage()
+   
+   @IBAction func onScan() {
+   
+       present(photoPicker, animated: false)
+
+   }
+   
 }
 
+
+extension HomeVC {
+  
+
+   func userSelectedPhoto(_ photo: UIImage) {
+      
+       self.selectedImage = photo
+       
+       DispatchQueue.global(qos: .userInitiated).async {
+           self.classifyImage(photo)
+       }
+   }
+
+}
+
+extension HomeVC {
+    
+   private func classifyImage(_ image: UIImage) {
+       do {
+           try self.imagePredictor.makePredictions(for: image,
+                                                   completionHandler: imagePredictionHandler)
+       } catch {
+           print("Vision was unable to make a prediction...\n\n\(error.localizedDescription)")
+       }
+   }
+
+  
+   private func imagePredictionHandler(_ predictions: [ImagePredictor.Prediction]?) {
+       guard let predictions = predictions else {
+           
+           DispatchQueue.main.async { [self] in
+               let vc = self.storyboard?.instantiateViewController(withIdentifier: "ItemDetailsVC") as! ItemDetailsVC
+               vc.image = selectedImage
+               vc.predictionText = "No Details"
+               self.navigationController?.pushViewController(vc, animated: true)
+              
+           }
+           
+           return
+       }
+
+       let formattedPredictions = formatPredictions(predictions)
+
+       let predictionString = formattedPredictions.joined(separator: "\n")
+       
+       DispatchQueue.main.async {
+           let vc = self.storyboard?.instantiateViewController(withIdentifier: "ItemDetailsVC") as! ItemDetailsVC
+           vc.image = self.selectedImage
+           vc.predictionText = predictionString
+           self.navigationController?.pushViewController(vc, animated: true)
+          
+       }
+       
+   }
+
+ 
+   private func formatPredictions(_ predictions: [ImagePredictor.Prediction]) -> [String] {
+       // Vision sorts the classifications in descending confidence order.
+       let topPredictions: [String] = predictions.prefix(predictionsToShow).map { prediction in
+           var name = prediction.classification
+
+           
+           if let firstComma = name.firstIndex(of: ",") {
+               name = String(name.prefix(upTo: firstComma))
+           }
+
+           return "\(name)"
+       }
+
+       return topPredictions
+   }
+}
 
 
 
