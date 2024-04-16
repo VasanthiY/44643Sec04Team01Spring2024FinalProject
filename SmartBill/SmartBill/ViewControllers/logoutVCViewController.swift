a2522c99785cb928c8dadce62259dc1a113f096d
kalpana762 <kalpanakanakamalla9@gmail.com>
2024-04-15 20:47:48 -0500

    kanakamalla kalpana created outlets

--- SmartBill/SmartBill/ViewControllers/logoutVCViewController.swift
+++ SmartBill/SmartBill/ViewControllers/logoutVCViewController.swift
@@ -9,28 +9,17 @@ import UIKit
 
 class logoutVCViewController: UIViewController {
 
-    @IBOutlet weak var displayEmail: UITextField!
     
-    @IBOutlet weak var displayName: UITextField!
-    
+    @IBOutlet weak var emailId: UITextField!
+    @IBOutlet weak var fullName: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func Logout(_ sender: UIButton) {
-        if displayEmail.text == "" {
-            
-            self.showAlert(str: "No email to display")
-            return
-        }
-        
-        if displayName.text == "" {
-            
-            self.showAlert(str: "No password to display")
-            return
-        }
+
+    @IBAction func logout(_ sender: Any) {
     }
     
     func showAlert(str: String) -> Void {
