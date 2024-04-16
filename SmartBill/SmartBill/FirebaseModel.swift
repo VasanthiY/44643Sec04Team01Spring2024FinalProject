b665f08b09ff8131e75da7f1fb5a950872fbee17
Jameer <jameer.jb@gmail.com>
2024-04-15 21:33:00 -0500

    Added Firebase logic to pull products and business logic for UploadImage Screen

--- SmartBill/SmartBill/FirebaseModel.swift
+++ SmartBill/SmartBill/FirebaseModel.swift
@@ -8,9 +8,23 @@
 import Foundation
 import FirebaseFirestore
 
+struct Product: Codable{
+    let name: String
+    let price: Double
+    let pic: String
+}
+
+struct Bill: Codable{
+    let items: String
+    let price: Double
+}
 
 struct FireStoreOperations {
     static let db = Firestore.firestore()
+    static var products: [Product] = []
+    static var productnames: [String] = []
+    static var productprices:[String:Double] = [:]
+    static var billsinfo:[Bill] = []
 
     public static func fetchUserName(emailId: String) async -> String {
         let userCollection = db.collection("userinfo")
@@ -34,5 +48,80 @@ struct FireStoreOperations {
             print("Error getting document: \(error)")
         }
         return "nil"
+    }
+    
+    public static func fetchProducts() async{
+        
+        let productcollection = db.collection("products")
+        do{
+            let productDocuments = try await productcollection.getDocuments().documents
+            
+            for product in productDocuments{
+                if product.exists {
+                    let productData = product.data()
+                    
+                    let name = productData["name"] as? String ?? "N/A"
+                    let pic = productData["pic"] as? String ?? "N/A"
+                    let price = productData["price"] as? Double ?? 0
+                    let prod = Product(name: name, price: price, pic: pic)
+                    products.append(prod)
+                } else{
+                    print("Document does not exist")
+                    
+                }
+            }
+        } catch {
+            print("Error getting document: \(error)")
+        }
+        
+    }
+    
+    public static func getProductNames() async{
+        await fetchProducts()
+        for product in products{
+            productnames.append(product.name.lowercased())
+        }
+    }
+    
+    public static func getProductPrice() async{
+        await fetchProducts()
+        for product in products {
+            productprices[product.name.lowercased()] = product.price
+        }
+    }
+    
+    public static func saveBill(data:[String : Any]){
+        
+        db.collection("bills").addDocument(data: data){ error in
+            if let error = error {
+                print("Error saving data: \(error.localizedDescription)")
+            } else {
+                print("Data saved successfully.")
+            }
+        }
+    }
+    
+    
+    public static func fetchBills() async{
+        let billcollection = db.collection("bills")
+        do{
+            let bills = try await billcollection.getDocuments().documents
+            
+            for bill in bills{
+                if bill.exists {
+                    let billData = bill.data()
+                    
+                    let items = billData["items"] as? String ?? "N/A"
+                    let price = billData["totalcost"] as? Double ?? 0
+                    let billinfo = Bill(items: items, price: price)
+                    billsinfo.append(billinfo)
+                } else{
+                    print("Document does not exist")
+                    
+                }
+            }
+        } catch {
+            print("Error getting document: \(error)")
+        }
     }
 }
--- SmartBill/SmartBill/ViewControllers/UploadImageVC.swift
+++ SmartBill/SmartBill/ViewControllers/UploadImageVC.swift
@@ -3,16 +3,104 @@
 //  SmartBill
 //
 //  Created by Krishna Vasanthi on 3/15/24.
+//  Modified by Jameer Babu on 3/31/24.
 //
 
 import UIKit
 
 class UploadImageVC: UIViewController {
 
+    var detectedObjects = [String]()
+    var matchedObjects = [String:Int]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let doubleTapGesture = UITapGestureRecognizer(target: self, action: #selector(doubleTapGesture(_:)))
+        doubleTapGesture.numberOfTapsRequired = 2
+        img.addGestureRecognizer(doubleTapGesture)
+        
+        img.isUserInteractionEnabled = true
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if(segue.identifier == "showBillDetails"){
+            let destinationVC = segue.destination as! BillDetailTVC
+            destinationVC.bills = matchedObjects
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selImg = info[.editedImage] as? UIImage {
+            AudioServicesPlaySystemSound(1109)
+            img.image = selImg
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func doubleTapGesture(_ gesture: UITapGestureRecognizer) {
+        AudioServicesPlaySystemSound(1104)
+        if gesture.state == .ended {
+            let imgCtrler = UIImagePickerController()
+            imgCtrler.delegate = self
+            imgCtrler.allowsEditing = true
+            imgCtrler.sourceType = .photoLibrary
+            present(imgCtrler, animated: true, completion: nil)
+        }
+    }
+    
+    
+    func performObjectDetection(_ image: UIImage) {
+        
+        guard let ciImage = CIImage(image: image) else {
+                    fatalError("Unable to create CIImage from UIImage")
+                }
+        
+        
+        
+        do {
+            // Perform the text-recognition request.
+            let imgHandler = VNImageRequestHandler(ciImage: ciImage)
+
+            let request = VNRecognizeTextRequest { (request, error) in
+                        guard let observations = request.results as? [VNRecognizedTextObservation] else { return }
+
+                        for currentObservation in observations {
+                            /// The 1 in topCandidates(1) indicates that we only want one candidate.
+                            /// After that we take our one and only candidate with the most confidence out of the array.
+                            let topCandidate = currentObservation.topCandidates(1).first
+
+                            //print(topCandidate?.string)
+                            self.detectedObjects.append(topCandidate!.string)
+                        }
+                    }
+            
+                    request.recognitionLanguages = ["en"]
+                    request.recognitionLevel = .accurate
+                    request.usesLanguageCorrection = true
+
+            try imgHandler.perform([request])
+        } catch {
+            print("Unable to perform the requests: \(error).")
+        }
+        
+    }
+    
+    
+    func matchObjects(){
+        let productsindb = FireStoreOperations.productnames
+        
+        let specialchars = "-"
+        
+        for detectedObject in detectedObjects {
+            let obj = detectedObject.lowercased().replacing(specialchars, with: "")
+            print("cleared:\(obj)")
+            if(productsindb.contains(obj)){
+                matchedObjects[obj, default: 0] += 1
+            }
+        }
     }
     
 
