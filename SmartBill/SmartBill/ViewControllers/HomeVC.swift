4fd01c9fc0222929aae6cbfc5b8f4509e282aaac
Krishna Vasanthi Yakkala <krishnavasanthiyakkala@gmail.com>
2024-04-15 20:17:40 -0500

    Added Firebase project

--- SmartBill/SmartBill/ViewControllers/HomeVC.swift
+++ SmartBill/SmartBill/ViewControllers/HomeVC.swift
@@ -17,10 +17,6 @@ class HomeVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
-        BillBTN.isEnabled = true
-        prevBillBTN.isEnabled = true
-        
     }
 
     /*
