551f61fe42cef869d4d15b8500632eab3e470d56
Krishna Vasanthi Yakkala <krishnavasanthiyakkala@gmail.com>
2024-04-17 13:08:58 -0500

    Added changes

--- SmartBill/SmartBill/ViewControllers/BillHistoryTVC.swift
+++ SmartBill/SmartBill/ViewControllers/BillHistoryTVC.swift
@@ -15,44 +15,44 @@ let price: Double
 class BillHistoryTVC: UITableViewController {
     var billitems:[Billitems]=[]
     override func viewDidLoad() {
-    super.viewDidLoad()
-    let bills = FireStoreOperations.billsinfo
-     
-    for bill in bills{
-    billitems.append(Billitems(items: bill.items, price: bill.price))
-    }
-    override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return billitems.count
-    }
-        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "History", for: indexPath)
-         
-        // Configure the cell...
-        let bill = billitems[indexPath.row]
-        cell.textLabel?.text = bill.items.description + " " + String(bill.price)
-         
-        return cell
+        let bills = FireStoreOperations.billsinfo
+        
+        for bill in bills{
+            billitems.append(Billitems(items: bill.items, price: bill.price))
         }
-    
-
+        func viewDidLoad() {
+            super.viewDidLoad()
+            
+            // Uncomment the following line to preserve selection between presentations
+            // self.clearsSelectionOnViewWillAppear = false
+            
+            // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+            // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        }
+        
+        // MARK: - Table view data source
+        
+        func numberOfSections(in tableView: UITableView) -> Int {
+            // #warning Incomplete implementation, return the number of sections
+            return 1
+        }
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            // #warning Incomplete implementation, return the number of rows
+            return billitems.count
+        }
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "History", for: indexPath)
+          
+            // Configure the cell...
+            let bill = billitems[indexPath.row]
+            cell.textLabel?.text = bill.items.description + " " + String(bill.price)
+            
+            return cell
+        }
+        
+    }
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
