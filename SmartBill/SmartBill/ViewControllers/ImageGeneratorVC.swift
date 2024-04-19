2c59c16bebad9e93cd0411795f60de7ba29656f0
Jameer <jameer.jb@gmail.com>
2024-04-19 12:59:43 -0500

    EDIT, DELETE with API
    1.  Added edit operation for bill - Leading.
    2. Added delete action for bill - Trailing.
    3. API

---
+++ SmartBill/SmartBill/ViewControllers/ImageGeneratorVC.swift
@@ -0,0 +1,29 @@
+//
+//  ImageGeneratorVC.swift
+//  SmartBill
+//
+//  Created by Jameer Babu on 4/19/24.
+//
+
+import UIKit
+
+class ImageGeneratorVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
